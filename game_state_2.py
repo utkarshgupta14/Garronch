e47a260ac66c4be7bba7911ddc4d6d633c9e90a4
Utkarsh Gupta <utkarshgupta@Utkarshs-MacBook-Pro.local>
2025-05-20 22:15:57 +0530

    added player entity and load_image util

---
+++ game_state_2.py
@@ -0,0 +1,52 @@
+import sys
+import pygame
+
+from scripts.entities import PhysicsEntity
+from scripts.utils import load_image 
+
+class Game():
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption('Garronch')
+        self.screen = pygame.display.set_mode((600, 480))
+
+        self.clock = pygame.time.Clock()
+
+        self.bg_color = (14, 150, 170)
+
+        self.movement = [False, False]
+
+        self.assets = {
+            'player' : load_image('entities/player.png')
+        }
+
+        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
+    
+    def run(self):
+        while True:
+            self.screen.fill(self.bg_color)
+
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.screen)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = False
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+
+Game().run()
