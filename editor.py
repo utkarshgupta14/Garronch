4eb6915c768ec264ab81ab8c07ad3afcbef1df0e
utkarshgupta14 <guptautkarsh1404@gmail.com>
2025-06-05 01:20:18 +0530

    added basic level editor

---
+++ editor.py
@@ -0,0 +1,134 @@
+import sys
+import pygame
+
+from scripts.utils import load_images
+from scripts.tilemap import Tilemap
+
+class Editor():
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption('Garronch')
+        self.screen = pygame.display.set_mode((640, 480))
+        self.display = pygame.Surface((320, 240))
+
+        self.RENDER_SCALE = 2
+
+        self.clock = pygame.time.Clock()
+
+        self.movement = [False, False, False, False]
+
+        self.assets = {
+            'decor' : load_images('tiles/decor'),
+            'grass' : load_images('tiles/grass'),
+            'large_decor' : load_images('tiles/large_decor'),
+            'stone' : load_images('tiles/stone'),
+        }
+        self.tile_list = list(self.assets)
+        self.tilemap = Tilemap(self, tile_size=16)
+
+        self.tile_group = 0
+        self.tile_variant = 0
+
+        self.scroll = [0, 0]
+        self.tile_display = pygame.Surface((24, 24))
+
+        self.clicking = False
+        self.right_clicking = False
+        self.shift = False
+    
+    def run(self):
+        while True:
+            self.display.fill(((173, 216, 230)))
+
+            # handle camera
+            self.scroll[0] += (self.movement[1] - self.movement[0]) * 2
+            self.scroll[1] += (self.movement[3] - self.movement[2]) * 2
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            # render places tiles
+            self.tilemap.render(self.display, offset=render_scroll)
+
+            # load current tile image
+            tile_img = self.assets[self.tile_list[self.tile_group]][self.tile_variant]
+            tile_img.set_alpha(150)
+
+            # display currect tile at mouse pos
+            mpos = pygame.mouse.get_pos()
+            mpos = (mpos[0] / self.RENDER_SCALE, mpos[1] / self.RENDER_SCALE)
+            tile_pos = (int((mpos[0] + self.scroll[0]) // self.tilemap.tile_size), int((mpos[1] + self.scroll[1]) // self.tilemap.tile_size))
+            self.display.blit(tile_img, (tile_pos[0] * self.tilemap.tile_size - self.scroll[0], tile_pos[1] * self.tilemap.tile_size - self.scroll[1]))
+
+            # add and remove tiles
+            if self.clicking:
+                self.tilemap.tilemap[str(tile_pos[0]) + ';' + str(tile_pos[1])] = {'type' : self.tile_list[self.tile_group], 'variant' : self.tile_variant, 'pos' : tile_pos}
+            if self.right_clicking:
+                tile_loc = str(tile_pos[0]) + ';' + str(tile_pos[1])
+                if tile_loc in self.tilemap.tilemap:
+                    del self.tilemap.tilemap[tile_loc]
+
+            # display selected tile
+            self.tile_display.fill((0, 0, 0))
+            tile_img.set_alpha(255)
+            self.tile_display.blit(pygame.transform.scale(tile_img, (16, 16)), (4, 4))
+            self.display.blit(self.tile_display, (5, 5))
+
+            # event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        self.clicking = True
+                    if event.button == 3:
+                        self.right_clicking = True
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if event.button == 1:
+                        self.clicking = False
+                    if event.button == 3:
+                        self.right_clicking = False
+
+                if event.type == pygame.MOUSEWHEEL:
+                    if self.shift:
+                        if event.y > 0 or event.x > 0: # macOS maps external mouse scroll to event.x when shift is pressed
+                            self.tile_variant = (self.tile_variant - 1) % len(self.assets[self.tile_list[self.tile_group]])
+                        if event.y < 0 or event.x < 0:
+                            self.tile_variant = (self.tile_variant + 1) % len(self.assets[self.tile_list[self.tile_group]])
+                    else:
+                        if event.y > 0:
+                            self.tile_group = (self.tile_group - 1) % len(self.tile_list)
+                            self.tile_variant = 0
+                        if event.y < 0:
+                            self.tile_group = (self.tile_group + 1) % len(self.tile_list)
+                            self.tile_variant = 0
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_a:
+                        self.movement[0] = True
+                    if event.key == pygame.K_d:
+                        self.movement[1] = True
+                    if event.key == pygame.K_w:
+                        self.movement[2] = True
+                    if event.key == pygame.K_s:
+                        self.movement[3] = True
+                    if event.key == pygame.K_LSHIFT:
+                        self.shift = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_a:
+                        self.movement[0] = False
+                    if event.key == pygame.K_d:
+                        self.movement[1] = False
+                    if event.key == pygame.K_w:
+                        self.movement[2] = False
+                    if event.key == pygame.K_s:
+                        self.movement[3] = False
+                    if event.key == pygame.K_LSHIFT:
+                        self.shift = False
+
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0,0))
+            pygame.display.update()
+            self.clock.tick(60)
+
+
+Editor().run()
--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -10,10 +10,6 @@ class Tilemap:
         self.game = game
         self.tilemap = {}
         self.offgrid_tiles = []
-
-        for i in range(10):
-            self.tilemap[str(3+i) + ";10"] = {'type' : 'grass', 'variant' : 1, "pos" : (3+i, 10)}
-            self.tilemap["10;" + str(5+i)] = {'type' : 'stone', 'variant' : 1, "pos" : (10, 5+i)}
         
     def tiles_around(self, pos):
         tiles_around = []
